06e50061a850b388646c79bc90799d6e7ccdf528
debady <debadychatue@gmail.com>
2025-11-05 10:56:07 +0000

    1er commit | initation projet

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MyFirstMauiApp;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent(); // relie le XAML au code-behind
+    }
+
+    private void OnGreetClicked(object sender, EventArgs e)
+    {
+        var name = NameEntry.Text ?? "ami";
+        GreetingLabel.Text = $"Bonjour, {name} 👋";
+    }
+
+    // private void OnCountClicked(object sender, EventArgs e)
+    // {
+    //     count++;
+    //     CountLabel.Text = $"Compteur: {count}";
+    // }
+}
